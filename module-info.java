ef2e7f9bf304a76e053d67f257eee0ede9689f26
Dapaaj <143654074+Dapaaj@users.noreply.github.com>
2026-08-10 23:18:19 +0100

    Add module-info.java for robot_gui module

---
+++ module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module robot_gui {
+        requires javafx.fxml;
+		requires javafx.graphics;
+		requires javafx.controls;
+		requires java.desktop;
+		exports robot_gui;
+	
+}
